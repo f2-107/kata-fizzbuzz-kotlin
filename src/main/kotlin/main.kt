1e9f01c22cf32781a1dce905ab6711c246adf7ee
f2-107 <florian.schneider@codecentric.de>
2021-02-24 12:08:56 +0100

    Refactor a bit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,23 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    var input: Int
+
+    do {
+        print("Enter an integer from 1-1000: > ")
+        input = readLine()!!.toInt()
+
+        if (input < 1 || input > 1000) println("\nError - invalid input")
+    } while (input < 1|| input > 1000)
+
+    for (i in 1 until input + 1) {
+        println("${i.toString().padStart(4, '0')}: ${fizzBuzz(i)}")
+    }
+}
+
+fun fizzBuzz(valueToCheck: Int): String {
+    when {
+        valueToCheck % 15 == 0 -> return "FizzBuzz"
+        valueToCheck % 3 == 0 -> return "Fizz"
+        valueToCheck % 5 == 0 -> return "Buzz"
+        else -> return valueToCheck.toString()
+    }
 }
